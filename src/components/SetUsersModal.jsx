7c30af6728363b0f4f4ef741a748dbedbaa1547f
Yehor-ka <kalolg0109@gmail.com>
2021-08-18 22:11:54 +0300

    update set users

--- src/components/SetUsersModal.jsx
+++ src/components/SetUsersModal.jsx
@@ -2,23 +2,41 @@ import React from 'react';
 import { Dialog, DialogContent, TextField, Button } from '@material-ui/core';
 
 const SetUsersModal = ({ isOpenBeginSetting, setUsersConfig, setIsOpenBeginSettings }) => {
-  const [UsersName, setUsersName] = React.useState({});
+  const [UsersName, setUsersName] = React.useState({
+    firstUsername: '',
+    secondUsername: ''
+  });
+
+  React.useEffect(() => {
+    if(UsersName.firstUsername.trim() === '') {
+      setUsersName((prevState) => ({
+        ...prevState,
+        firstUsername: 'Player1',
+      }));
+    }
+    if(UsersName.secondUsername.trim() === '') {
+      setUsersName((prevState) => ({
+        ...prevState,
+        secondUsername: 'Player2',
+      }));
+    }
+  }, [UsersName])
 
   const onChangeInput = (e) => {
-    const name = e.target.name
-    const value = e.target.value
-    setUsersName(prevState => ({
-        ...prevState,
-        [name]: value
-    }))
-  }
+    const name = e.target.name;
+    const value = e.target.value;
+    setUsersName((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const handleAddPlayers = () => {
-    setUsersConfig(prevState => ({
-        ...prevState,
-        ...UsersName
-    }))
-    setIsOpenBeginSettings(false)
+    setUsersConfig((prevState) => ({
+      ...prevState,
+      ...UsersName,
+    }));
+    setIsOpenBeginSettings(false);
   };
 
   return (
@@ -31,8 +49,18 @@ const SetUsersModal = ({ isOpenBeginSetting, setUsersConfig, setIsOpenBeginSetti
           fullWidth
           onChange={onChangeInput}
         />
-        <TextField name="secondUsername" label="Second Username" type="text" fullWidth onChange={onChangeInput} />
-        <Button style={{marginTop: 15}} variant="contained" color="secondary" onClick={handleAddPlayers}>
+        <TextField
+          name="secondUsername"
+          label="Second Username"
+          type="text"
+          fullWidth
+          onChange={onChangeInput}
+        />
+        <Button
+          style={{ marginTop: 15 }}
+          variant="contained"
+          color="secondary"
+          onClick={handleAddPlayers}>
           Add players
         </Button>
       </DialogContent>
