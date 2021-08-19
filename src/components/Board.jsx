bfd978eea3873f45bebbf1d0a85a6eafe374dd6d
Yehor-ka <kalolg0109@gmail.com>
2021-08-19 15:20:37 +0300

    update styles

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Square from './Square';
 
-const Board = ({ board, handleStep, winPos, lineThroughStyle }) => {
+const Board = ({ board, handleStep, lineThroughStyle }) => {
   return (
     <div className="board">
       <div className="board__line-through" style={lineThroughStyle}></div>
